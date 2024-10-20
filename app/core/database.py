4af8436d4100f9e1a0a01d81a3f0bb7b6d6405ed
jdgilhuly <jdgilhuly@icloud.com>
2024-10-19 17:20:16 -0700

    refactor database

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,9 @@
-from databases import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://your_username:your_password@localhost:5432/event_management"
 
-database = Database(DATABASE_URL)
-metadata = MetaData()
-
 engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
