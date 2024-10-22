850ad9d892788aa80a09cfbcd01cadc53dead7bd
jdgilhuly <jdgilhuly@icloud.com>
2024-10-22 09:30:13 -0700

    fixing dockerfile

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ RUN pipenv install --system --deploy
 EXPOSE 8000
 
 # Run the application
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["pipenv", "run", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # After creating the user and database, update your application's database connection string.
-DATABASE_URL = "postgresql://tester:georgie@localhost:5432/test"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://tester:georgie@localhost:5432/test")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
