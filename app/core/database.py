0bcf0705b4abbf831b93210df83c495790852598
jdgilhuly <jdgilhuly@icloud.com>
2024-10-22 09:50:07 -0700

    docker-compose working

--- app/core/database.py
+++ app/core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # After creating the user and database, update your application's database connection string.
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://tester:georgie@localhost:5432/test")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://tester:georgie@db:5432/test")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
